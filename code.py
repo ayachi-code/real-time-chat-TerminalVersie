574806e8d9889bc3366c15030c54650ebd20705c
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-21 20:34:26 +0100

    elke keuze word een functie

--- code.py
+++ code.py
@@ -5,6 +5,12 @@ class welkom:
     print("Welkom bij chat-app-by-bilal")
     print("1 inloggen 2 registreren")
     groet = input("Type een keuze ")
+    if groet == "1":
+        def inloggen():
+            pass
+    else if groet == "2":
+            registreren()
+            pass
 
 
 
