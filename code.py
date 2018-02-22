3600ff68a665127a04c0f4ff075fbed702c92033
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-22 09:44:43 +0100

    chat class veranderd naar chat

--- code.py
+++ code.py
@@ -1,19 +1,13 @@
 import os
 import socket
 
-class welkom:
+class chat:
     print("Welkom bij chat-app-by-bilal")
     print("1 inloggen 2 registreren")
     groet = input("Type een keuze ")
-    if groet == "1":
-        def inloggen():
-            pass
-    else if groet == "2":
-            registreren()
-            pass
 
 
 
 
 
-home = welkom()
+home = chat()
