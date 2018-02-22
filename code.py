07c32c2e435a138609d4c2082ca4bb4ef05508be
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-22 10:09:20 +0100

    als je een keuze maakt dan krijg je de keuze uitgeprint

--- code.py
+++ code.py
@@ -2,12 +2,17 @@ import os
 import socket
 
 class chat:
-    print("Welkom bij chat-app-by-bilal")
-    print("1 inloggen 2 registreren")
-    groet = input("Type een keuze ")
+    def __init__():
+         global groet
+
+    def home():
+        print("Welkom bij chat-app-by-bilal")
+        print("1 inloggen 2 registreren")
+        groet = input("Type een keuze ")
+        if groet == "1":
+            print("inloggen")
+        elif groet == "2":
+            print("registreren")
 
 
-
-
-
-home = chat()
+home = chat.home()
