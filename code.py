3c38cad980363715bd566f8a46654ba8b210c1fc
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-22 10:25:35 +0100

    inloggen functie verwijderd

--- code.py
+++ code.py
@@ -5,9 +5,6 @@ class chat:
     def __init__():
          global groet
 
-    def inloggen():
-        print("Welkom bij")
-
     def home():
         print("Welkom bij chat-app-by-bilal")
         print("1 inloggen 2 registreren")
